8430773ea7c7896036c67704bb57d5b73cbb984f
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-09-05 18:00:38 -0500

    Add file menu.js

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+const openMenu = document.querySelector('.box-menu');
+const modalMenu = document.querySelector('.box-menu-1');
+openMenu.addEventListener('click', () => {
+  //console.log('hello world');
+  modalMenu.style.left = 0;
+});
