b8d5dbb720577bdf2e352986165cc0b1cb4798de
Osvaldo Barrios Cortina <osvaldo.barrios@gmail.com>
2022-09-06 14:10:23 -0500

    Add close menu

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,11 @@
 const openMenu = document.querySelector('.box-menu');
 const modalMenu = document.querySelector('.box-menu-1');
 openMenu.addEventListener('click', () => {
-  //console.log('hello world');
   modalMenu.style.left = 0;
+});
+
+const closeMenu = document.querySelector('.close_menu');
+//const modalClose = document.querySelector('.box-menu-1');
+closeMenu.addEventListener('click', () => {
+  modalMenu.style.left = '-100%';
 });
